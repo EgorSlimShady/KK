aa7f2c21f93c3d7af214532453dc0dce56107259
EgorSlimSHady <Egor.tipes29@gmail.com>
2023-09-13 14:40:44 +0700

    test pay

---
+++ test_pay/js3.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+
+let validCode = '';
+
+app.get('/videoStream', (req, res) => {
+    const videoUrl = 'https://live.katun24.ru:8082/18ed1c64-2ec7-4c8c-bf52-14bea4ead6d0';
+
+    request(videoUrl).pipe(res);
+});
+
+app.get('/getCode', (req, res) => {
+    const currentDate = new Date();
+    const expirationDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 1);
+
+    if (currentDate <= expirationDate) {
+        validCode = generateCode();
+        res.send(validCode);
+    } else {
+        res.send('Код больше не действителен');
+    }
+});
+
+app.get('/checkCode/:code', (req, res) => {
+    const code = req.params.code;
+
+    if (code === validCode) {
+        res.send('Код действителен');
+    } else {
+        res.send('Неверный код');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Сервер запущен на порту 3000');
+});
+
+function generateCode() {
+    let code = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const codeLength = 6;
+
+    for (let i = 0; i < codeLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        code += characters.charAt(randomIndex);
+    }
+
+    return code;
+}
